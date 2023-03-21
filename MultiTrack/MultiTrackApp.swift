dd8d25d99b3dd43632453d8e7324589cfbc1bb1c
Andrew Hoglund <andrew@andrewhoglund.com>
2023-03-21 01:04:40 -0500

    Initial Commit

---
+++ MultiTrack/MultiTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiTrackApp.swift
+//  MultiTrack
+//
+//  Created by Drew on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MultiTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
